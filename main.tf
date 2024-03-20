e4309f2d24647e4f92d486f9feb4e583603a30be
Hemantjangir53 <jangirhemant53@gmail.com>
2024-03-20 17:18:26 +0530

    update out code

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "vpc" {
     source = "./module/aws_vpc"
     aws_region = local.aws_region
-   # vpc_name = var.vpc_name
+   
     vpc_name = local.vpc_names   # comes from root module valiables.tf file
     vpc_cidr = local.vpc_cidr
-    
+ #subnets
     availability_zones = local.availability_zones
     public_subnet_cidr_blocks = local.public_subnet_cidr_blocks
     private_subnet_cidr_blocks = local.private_subnet_cidr_blocks
